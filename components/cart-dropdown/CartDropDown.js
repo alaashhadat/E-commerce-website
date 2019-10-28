1cf26ced7e99510826e19e984a18b4b0d7a6e7d0
mac <mac@AlaaMacBookPro.local>
2019-10-28 13:13:33 +0100

    cart component

---
+++ components/cart-dropdown/CartDropDown.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CustomButton from "../custom-button/CustomButton";
+import "./CartDropDown-styles.scss";
+const Cart = () => (
+  <div className="cart-dropdown">
+    <CustomButton>Go to Checkout</CustomButton>
+  </div>
+);
+export default Cart;
--- components/header/Header.js
+++ components/header/Header.js
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { connect } from "react-redux";
-
+import CartDropDown from "../cart-dropdown/CartDropDown";
 import "./Header-sttyles.scss";
+import CartIcon from "../cart-item/CartIcon";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -26,10 +27,13 @@ const Header = ({ currentUser }) => (
       ) : (
         <Link to="/sign-in">Sign in </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? <CartDropDown /> : null}
   </div>
 );
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 export default connect(mapStateToProps)(Header);
